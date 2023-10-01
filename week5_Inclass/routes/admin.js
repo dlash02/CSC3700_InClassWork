e3149e48a92aa19b69b483828e6023a762a3864c
David Lash <dlash02@gmail.com>
2023-10-01 10:07:18 -0500

    Update from 9/29
    Update from 9/29

--- week5_Inclass/routes/admin.js
+++ week5_Inclass/routes/admin.js
@@ -3,20 +3,18 @@ const router = express.Router();
 const path = require('path');
 let products = [];
 router.get(  '/add-product', (req, res, next) => {
-    let h = "<html> <body>";
-    let h2 = "<h2> Add New Product </h2>";
-    let f = "<form action='/product' method='post'>";
-    let inBox = "<input type='text' name='title'/>";
-    let but = "<button type='submit'>Add Product</button>"
-    let fe = "</form> </body></html>";
-    res.send(`${h} ${h2} ${f} ${inBox} ${but} ${fe}`);
+    res.render( 'addProduct',
+        {
+            from: 'addProduct'
+        })
 });
 router.post(  '/product', (req, res, next) => {
     // console.log( req );
     console.log("----flag")
     let t = req.body.title;
     products.push( {title: t})
-    res.send(`Made it to post title:${t}`);
+    res.redirect('/add-product')
+    // res.send(`Made it to post title:${t}`);
 });
 
 // module.exports = router;
--- week5_Inclass/routes/shop.js
+++ week5_Inclass/routes/shop.js
@@ -5,6 +5,11 @@ const adminData = require("./admin");
 router.get(  '/showProducts', (req, res, next) => {
     console.log( "----------------");
     console.log( adminData.products);
-    res.sendFile( ( path.join(__dirname, '../views', 'showProducts.html')));
+    //res.sendFile( ( path.join(__dirname, '../views', 'showProducts.html')));
+    res.render( 'showProducts', {
+        title: "Show Available Products",
+        from: 'showProducts',
+        products : adminData.products
+    });
 });
 module.exports = router;

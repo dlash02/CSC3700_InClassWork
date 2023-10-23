b3ceef4634f6d42939a5da6bc2e517d3084e0eb1
David Lash <dlash02@gmail.com>
2023-10-23 11:46:52 -0500

    Update Data for CRUD

---
+++ Week9_inClass/models/product.js
@@ -0,0 +1,28 @@
+const db = require("../util/database");
+
+module.exports = class Product {
+    constructor( t, a, price ) {
+        this.title = t;
+        this.author = a;
+        this.price = price;
+        // this.description = "It was good it was bad it was ugly";
+    }
+    save() {
+            return db.execute( 'insert into products (title, price, author) ' +
+                'values (?, ?, ?)',
+                [this.title, this.price, this.author ]
+            )
+    }
+    static delete( id ) {
+        return db.execute( "delete from products where id = ?",
+            [id]
+        )
+    }
+    static fetchAll(){
+      return db.execute( "select * from products");
+    }
+    static findById( id ){
+        return db.execute( "select * from products where id = ?",
+            [id] );
+    }
+}

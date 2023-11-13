142e263baab273c5f03105986ae1e98bae81a0b2
David Lash <dlash02@gmail.com>
2023-11-13 06:50:59 -0600

    React update

--- Week10/Week11/my-app-prep/src/Books.js
+++ Week10/Week11/my-app-prep/src/Books.js
@@ -5,21 +5,36 @@ import {Button} from "bootstrap-react";
 
 function Books () {
     const[ books, setBooks ] = useState( null );
-
+    const [error, setError ] = useState(  null );
     const handleDelete = ( id ) => {
         const newBooks = books.filter( b => b.id != id );
         setBooks( newBooks );
     }
+
+    const [ isPending, setIsPending] = useState(true);
     useEffect( () => {
         let url = " http://localhost:8000/books";
-        fetch( url )
-            .then(resp => {
-                return resp.json()
-            }).
+        console.log("URL->" + url );
+        setTimeout( () => {
+            fetch( url )
+                .then(resp => {
+                    // console.log( `resp->`); console.log( resp );
+                    if ( !resp.ok){
+                        throw Error("Cannot fetch URL data for resource")
+                    }
+                    return resp.json()
+                }).
             then(data => {
+                setIsPending( false );
                 console.log( data );
                 setBooks( data );
+                setError ( null );
+            }).catch( (err) => {
+                console.log("Error:"); console.log(err.message);
+                setIsPending( false );
+                setError( err.message);
             })
+        }, 2000)
 
     }, [] );
     const myTitle = "Books 4 u 4 Sale!"
@@ -29,6 +44,8 @@ function Books () {
             <Col sm={3}>
             </Col>
             <Col sm={3}>
+                { error && <div> Error: {error} </div> }
+                {isPending && <div> Loading ...</div>}
                 { books && <BookList books={books} title={myTitle} handleDelete={handleDelete} />}
             </Col></Row>
     )

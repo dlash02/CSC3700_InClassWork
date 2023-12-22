878094a46ddff5264c756cd63d3be007f9a237de
David Lash <dlash02@gmail.com>
2023-12-22 14:18:48 -0600

    Update

--- week14/router-work/src/components/Profile.js
+++ week14/router-work/src/components/Profile.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import {useAuth} from "./auth";
+import {useNavigate} from "react-router-dom";
+import {Button} from "react-bootstrap";
 
 function Profile(props) {
+    const auth = useAuth();
+    const navigate = useNavigate();
+    const handleLogout = () => {
+        auth.logOut();
+        navigate('/');
+    }
     return (
         <div>
-         <h2> Welcome Human to your profile page </h2>
+         <h2> Welcome {auth.user} to your profile page </h2>
+            <Button onClick={handleLogout}> Logout </Button>
         </div>
     );
 }
